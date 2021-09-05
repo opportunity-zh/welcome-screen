ea62155b13b3e66543958fd777e72ab7916792c4
Tuğçe Nur Taş <tnt@panter.ch>
2021-09-05 14:51:30 +0200

    feat: first draft of welcome screen

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,155 @@
 <template>
   <div id="app">
-  {{ title }}
+    <h1 class="site-title">{{ title }}</h1>
+    <span class="site-description">{{ currentDate }}</span>
 
-  <ul>
-    <li v-for="entry in entries" :key="entry.id">
-    {{ entry.description }}
-    </li>
-  </ul>
+    <!-- entry list -->
+    <ul class="entry-list">
+      <li
+        v-for="entry in filteredEntries"
+        :key="entry.id"
+        class="entry-item"
+      >
+        <span class="entry-daytime">
+          {{ entry[0] }} Uhr, {{ entry[1].replaceAll("/", ".") }} </span><br />
+        <h3 class="entry-title">{{ entry[2] }}</h3>
+        <span class="entry-description">{{ entry[3] }}</span><br />
+      </li>
+    </ul>
+
+    <!-- footer -->
+    <footer class="footer">
+      <img
+        src="./assets/logo_stzh_sd_rgb_blau_digital.svg"
+        alt="Stadt Zürich Soziale Betriebe Logo"
+      >
+      <img
+        src="./assets/Opportunity.png"
+        alt="Opportunity Zürich Logo"
+      >
+      <img
+        src="./assets/SAG_Logo_De.png"
+        alt="Stiftung Arbeitsgestaltung Logo"
+      >
+    </footer>
   </div>
 </template>
 
 <script>
+import axios from "axios"; // axios is a library for making HTTP requests to the backend
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       title: "Welcome to Opportunity",
-      entries: [
-        {
-          id: 1,
-          description: "Basisbeschäftigung Besuch",
-          daytime: "14.00 Uhr"
-        },
-        {
-          id: 2,
-          description: "Sozialamt Dietikon",
-          daytime: "15.30 Uhr"
-        }
-      ]
-    }
-  }
-}
+      gsheet_url:
+        "https://sheets.googleapis.com/v4/spreadsheets/15_C4Rx7692L5WNy4y4DEksZKDM0HRjrcUuXVTMtElwI/values:batchGet?ranges=A1%3AE100&valueRenderOption=FORMATTED_VALUE&key=AIzaSyBesotaNgSaTUIhrSKjEaExdi-ksKInhoE",
+      entries: [],
+      currentDate: "",
+    };
+  },
+  computed: {
+    // computed properties are like data properties, but with a method combined and it gets executed automatically, instead of calling a function explicitly
+    filteredEntries() {
+      return [...this.entries].slice(1); // remove the first item from the array
+    },
+  },
+  methods: {
+    getData() {
+      axios.get(this.gsheet_url).then((response) => {
+        this.entries = response.data.valueRanges[0].values;
+      });
+    },
+    updateCurrentDate() {
+      let today = new Date();
+      const currentDate = `${today.getDate()}.${today.getMonth()}.${today.getFullYear()}`;
+      this.currentDate = currentDate;
+    },
+    refreshData() {
+      this.getData();
+      this.updateCurrentDate();
+    },
+  },
+  mounted() {
+    this.refreshData(); // get first initial data and then wait for the next update
+    setInterval(() => {
+      this.refreshData();
+    }, 1800000); // wait 30mins for next update
+  },
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap");
+
+body {
+  background: #e8eff4;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #323d4a;
+  margin: 60px;
+}
+
+.site-title {
+  font-size: 82px;
+  font-weight: 900;
+  margin: 80px 0 20px 0;
+}
+
+.site-description {
+  font-size: 82px;
+  color: #9aa7b1;
+  font-weight: 500;
+  margin: 0;
+}
+
+.entry-list {
+  padding-left: 0;
+}
+
+.entry-item {
+  padding: 40px;
+  margin: 50px 0;
+  background: #0f05a0;
+  font-size: 38px;
+  line-height: 1.4;
+  list-style: none;
+}
+
+.entry-daytime {
+  color: #eb5e00;
+  font-weight: 900;
+}
+
+.entry-title {
+  font-size: inherit;
+  margin: 0;
+  color: #ffbfab;
+  font-weight: 900;
+}
+
+.entry-description {
+  color: #ffbfab;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 60px;
+  background: #fff;
+}
+
+.footer img {
+  height: 60px;
 }
 </style>
